f31a5c570d67befefb4b45509f9ba4f5bc9d5790
A. Korobetskaya <kornast@yandex.ru>
2021-09-28 19:36:56 +0400

    Пример "Проверка возраста" - доработка на лекции

--- CheckAge/ProgramCheckAge.cs
+++ CheckAge/ProgramCheckAge.cs
@@ -9,34 +9,54 @@ namespace CheckAge
             Console.WriteLine("Введите возраст (полных лет)");
             string input = Console.ReadLine();
             int age;
-            Console.Write("Статус: ");
-            if (int.TryParse(input, out age) && age > 0 && age <= 150)
+            if (TryParseAge(input, out age))
             {
-                string adult = (age >= 18) ? "совершеннолетний" : "несовершеннолетний";
+                string adult = AdultToString(IsAdult(age));
+                string category = GetCategory(age);
+                bool hasAnniversary = HasAnniversary(age);
 
-                string category = "";
-                if (age < 7)
-                    category = "дошкольник";
-                else if (age <= 18)
-                    category = "школьник";
-                else if (age >= 60)
-                    category = "пенсионер";
-                else
-                    category = "трудящийся";
-
-                bool hasAnniversary = age % 10 == 0 || age > 50 && age % 5 == 0;
-
-                Console.Write(adult + " " + category);
-                if (hasAnniversary)
-                {
-                    Console.Write(", юбиляр");
-                }
-                Console.WriteLine(".");
+                Console.WriteLine(GetResult(adult, category, hasAnniversary));
             }
             else
             {
                 Console.WriteLine("Введенное значение не может быть интерпретировано как возраст.");
             }
+        }
+
+        static bool IsAdult(int age)
+        {
+            return age >= 18;
+        }
+
+        static string AdultToString(bool adult)
+        {
+            return adult ? "совершеннолетний" : "несовершеннолетний";
+        }
+
+        static string GetCategory(int age)
+        {
+            if (age < 7)
+                return "дошкольник";
+            if (age <= 18)
+                return "школьник";
+            if (age >= 60)
+                return "пенсионер";
+            return "трудящийся";
+        }
+
+        static bool HasAnniversary(int age)
+        {
+            return age % 10 == 0 || age > 50 && age % 5 == 0;
+        }
+
+        static string GetResult(string adult, string category, bool hasAnniversary)
+        {
+            return "Статус: " + adult + " " + category + (hasAnniversary ? ", юбиляр" : "") + ".";
+        }
+
+        static bool TryParseAge(string stringAge, out int age)
+        {
+            return int.TryParse(stringAge, out age) && age >= 0 && age <= 150;
         }
     }
 }
